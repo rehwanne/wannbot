98ce29481ad3fcb4511f826027cf76fa4a8d353b
Sascha kruse <kruse@praemandatum.de>
2016-12-15 14:34:42 +0100

    rredmine use tabulate to create table

--- rredmine.py
+++ rredmine.py
@@ -5,6 +5,7 @@ import sys
 import traceback
 
 import redmine
+from tabulate import tabulate
 
 from response import Response
 import config
@@ -16,41 +17,53 @@ if config.redmine_key:
 else:
     redmine = None
 
+
 def link_redmine(mattermost_request):
     text = mattermost_request.text
     answer = ""
     text = ''.join(text).encode('latin1')
 
-    m = re.findall('#([0-9]+)', text)
-    if (len(m) < 1):
+    ids = re.findall('#([0-9]+)', text)
+
+    if (len(ids) < 1):
         return Response("no issue IDs found")
-    else:
-        for issue in m:
-            url = config.redmine_url + "issues/" + issue
-            if not redmine:
-                answer += url + "\n"
-            else:
-                answer += "| link | {} | \n".format(url)
-                answer += "|:---:|:---:| \n"
-                try:
-                    i = redmine.issue.get(issue)
-                except ResourceNotFoundError as e:
-                    answer += "| **ERROR** | Issue not found: #{} |\n".format(issue)
-                    traceback.print_exc(file=sys.stdout)
-                    answer += "\n"
-                    continue
-                except:
-                    answer += "| **ERROR** | Unable to get data for issue: #{} |\n".format(issue)
-                    traceback.print_exc(file=sys.stdout)
-                    answer += "\n"
-                    continue
-                answer += "| subject | {} | \n".format(i.subject.encode('utf-8'))
-                answer += "| author | {} | \n".format(i.author.name.encode('utf-8'))
-                if not hasattr(i, 'assigned_to'):
-                    name = "Nobody"
-                else:
-                    name = i.assigned_to.name.encode('utf-8')
-                answer += "| assigned to | {} | \n".format(name)
-                answer += "| status | {} | \n".format(i.status.name.encode('utf-8'))
-            answer += "\n"
+
+    answer = ""
+    for issue in ids:
+        issue_data = get_data(issue)
+        header = issue_data[0]
+        rows = issue_data[1:]
+        answer += tabulate(rows, header, tablefmt="pipe")
+        answer += "\n"
+
     return Response(answer)
+
+def get_data(issue_id):
+    data = []
+    url = config.redmine_url + "issues/" + str(issue_id)
+    data.append(["link", url])
+
+    if not redmine:
+        return data
+
+    try:
+        i = redmine.issue.get(issue_id)
+    except ResourceNotFoundError as e:
+        data.append(["**ERROR**", "issue_id not found ${}".format(issue_id)])
+        traceback.print_exc(file=sys.stdout)
+        return data
+    except:
+        data.append(["**ERROR**", "Unable to get data for issue_id ${}".format(issue_id)])
+        traceback.print_exc(file=sys.stdout)
+        return data
+
+    data.append(["subject", i.subject])
+    data.append(["author", i.author.name])
+
+    if not hasattr(i, 'assigned_to'):
+        name = "Nobody"
+    else:
+        name = i.assigned_to.name
+    data.append(["assigned to", name])
+    data.append(["status", i.status.name])
+    return data
